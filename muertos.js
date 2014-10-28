b34c3ec5fbcc0990b16aa2213ffef293abe6c7e5
Andrew Metcalf <andrew@luminal.io>
2014-10-27 23:09:51 -0400

    More fixes

--- muertos.js
+++ muertos.js
@@ -24,7 +24,7 @@ var initFrames = function(numLEDs) {
 		Color().hsv(25,100,100),
 		Color().hsv(50,100,100),
 		Color().hsv(340,80,100),
-		Color().hsv(260,100,50)
+		Color().hsv(260,100,100)
 	];
 
 	for(var frame=0; frame<numFrames; frame++) {
