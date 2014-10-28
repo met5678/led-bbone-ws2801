766010b5724d2c5554702d7a8b90dfae9ce04a9b
Andrew Metcalf <andrew@luminal.io>
2014-10-28 01:23:24 -0400

    Fixes

--- muertos.js
+++ muertos.js
@@ -15,12 +15,10 @@ var generateFrames = function(numLEDs,numFrames) {
 		Color().hsv(0,0,100),
 		Color().hsv(0,0,100),
 		Color().hsv(0,0,100),
-		Color().hsv(0,0,100),
 		Color().hsv(0,100,100),
 		Color().hsv(25,100,100),
 		Color().hsv(50,100,100),
 		Color().hsv(340,80,100),
-		Color().hsv(260,100,100)
 	];
 
 
@@ -29,21 +27,24 @@ var generateFrames = function(numLEDs,numFrames) {
 	var waveLength = 10;
 	var waveWidth = 4;
 
-	var drawPoint = function(frame,loc,color,width) {
+	var easings = Easing(100,'circular',{endToEnd:true});
+
+	var drawPoint = function(frame,loc,width,color,value) {
 		var startLED = Math.floor(loc-(width/2));
 		var endLED = Math.ceil(loc+(width/2));
 
 		for(var curLED=startLED; curLED <= endLED; curLED++) {
+			var calcColor = color.clone().value(value*100);
 			var normLED = curLED < 0 ? numLEDs+curLED : curLED%numLEDs;
 			var ledColor = Color().rgb(frame[normLED]);
 
 			var dist = (Math.abs(loc-curLED)*2)/width;
 			if(dist > 1) dist=1;
 			if(dist < 0) dist=0;
-			var val = Math.floor(Math.pow(1-dist,2)*100);
-			waveColor.value(val*val);
 
-			ledColor.mix(waveColor);
+			calcColor.darken(easings[Math.floor(dist*50)]);
+
+			ledColor.mix(calcColor,value);
 
 			frame[normLED] = ledColor.rgbArray();
 		}
@@ -54,13 +55,10 @@ var generateFrames = function(numLEDs,numFrames) {
 		var startLED = Math.floor(Math.random()*numLEDs);
 		var startFrameIndex = Math.floor(Math.random()*numFrames);
 
-		var easings = Easing(100,'circular',{endToEnd:true});
 
 		var moveAmount = waveLength/waveFrames;
 
 		var waveColor = colors[Math.floor(Math.random()*colors.length)].clone();
-
-
 
 		for(var waveFrameIndex=0; waveFrameIndex<waveFrames; waveFrameIndex++) {
 			var realFrameIndex = (waveFrameIndex+startFrameIndex)%numFrames;
@@ -69,9 +67,10 @@ var generateFrames = function(numLEDs,numFrames) {
 				var curCenter = startLED + waveFrameIndex*moveAmount;
 			else
 				var curCenter = startLED - waveFrameIndex*moveAmount;
-			waveColor.value(easings[waveFrameIndex]*100);
 
-			drawPoint(frames[realFrameIndex],curCenter,waveColor,waveWidth);
+			var value = easings[waveFrameIndex];
+
+			drawPoint(frames[realFrameIndex],curCenter,waveWidth,waveColor,value);
 		}
 
 		console.log("Finished wave " + wave);
