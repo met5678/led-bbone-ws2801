42fd7e1157ddb7d1843977e27d04f4b672e1ea14
Andrew Metcalf <met5678@gmail.com>
2014-10-31 01:07:05 +0000

    Commit

---
+++ main-brg.js
@@ -0,0 +1,54 @@
+try {
+	var SPI = require('spi');
+	var spi = new SPI.Spi('/dev/spidev1.0');
+
+	spi.maxSpeed(1000000);
+	spi.open();
+} catch(e) {
+	console.log("Fake mode");
+	var spi = {
+		write:function(){}
+	};	
+}
+
+var numLEDs = !!process.argv[3] ? Number(process.argv[3]) : 20;
+var effect = !!process.argv[2] ? process.argv[2] : 'none';
+var msPerFrame = !!process.argv[5] ? Number(process.argv[4]) : 16;
+
+var numFrames = 840;
+
+if(~effect.indexOf('.json')) {
+	var frames = require('./generated/'+effect);
+	numLEDs = frames[0].length;
+}
+else {
+	var frameGenerator = require('./'+effect);
+	frames = frameGenerator.generateFrames(numLEDs,numFrames);
+}
+
+var convertToBuffers = function() {
+	for(var frame=0; frame<numFrames; frame++) {
+		var buf = new Buffer(numLEDs*3);
+		for(var led=0; led<numLEDs; led++) {
+			var byteIndex = led*3;
+			buf[byteIndex] = frames[frame][led][2];
+			buf[byteIndex+1] = frames[frame][led][0];
+			buf[byteIndex+2] = frames[frame][led][1];
+		}
+
+		frames[frame] = buf;
+	}
+};
+
+convertToBuffers();
+
+var curFrame = 0;
+
+var doFrame = function() {
+	var buf = frames[curFrame];
+	spi.write(buf);
+
+	curFrame = curFrame >= numFrames-1 ? 0 : curFrame+1;
+}
+
+myInterval = setInterval(doFrame,msPerFrame);
